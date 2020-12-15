3d1a959d3105a19c80c1998250691b5f1f772fe4
glouis75 <75375660+glouis75@users.noreply.github.com>
2020-12-15 17:37:08 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ kubectl apply -f database/database.yaml
 kubectl apply -f database/database-service.yaml
 kubectl apply -f wordpress/wordpress.yaml
 kubectl apply -f wordpress/wordpress-service.yaml
-kubectl apply -f phpmyadmin/phpmyadmin.yaml
-kubectl apply -f phpmyadmin/phpmyadmin-service.yaml
+kubectl apply -f pma/phpmyadmin.yaml
+kubectl apply -f pma/phpmyadmin-service.yaml
 echo "[info] All deployed !"
 echo "-------------Service List-------------"
 minikube service list
