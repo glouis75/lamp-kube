cf3b68d8d1244527145e29cf13c777e44b94c360
glouis75 <75375660+glouis75@users.noreply.github.com>
2020-12-16 10:05:11 +0100

    Update undeploy.sh

--- undeploy.sh
+++ undeploy.sh
@@ -1,5 +1,6 @@
 kubectl scale --replicas=0 deployment --all
 kubectl delete all --all
+kubectl delete -n default ingress wordpress-http-ingress
+kubectl delete -n default ingress phpmyadmin-http-ingress
+kubectl delete -n default persistentvolumeclaim data-database-0
 echo "[info] Everything has been cleaned !"
-
-
